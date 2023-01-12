519606adbf0f901035530a75caa28ab87ecaafcb
jergerd <109340374+jergerd@users.noreply.github.com>
2023-01-12 15:18:10 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,30 @@
+/**
+ * Functions are mapped to blocks using various macros
+ * in comments starting with %. The most important macro
+ * is "block", and it specifies that a block should be
+ * generated for an **exported** function.
+ */
 
+//% color="#14de46" weight=100
+namespace hello {
+    //% block
+    export function helloWorld() {
+        basic.showString("Hello World")
+        basic.pause(2000)
+    }
+
+    // note that Caml casing yields lower case
+    // block text with spaces
+
+    //% block
+    export function number42(): number {
+        let answer = 38+4
+        return answer 
+    }
+
+    //% block
+    export function numberRand(): number {
+        let rand = Math.floor(Math.random() * 11)
+        return rand 
+    }
+}
